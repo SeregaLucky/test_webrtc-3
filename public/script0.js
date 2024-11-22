c0e7e64ea2db6720264a387da45354bc003f6f3a
SeregaLucky <strizgsergey@gmail.com>
2024-11-22 14:53:35 +0200

    add some logs

--- public/script0.js
+++ public/script0.js
@@ -24,13 +24,7 @@ async function start() {
     localStream.getTracks().forEach((track) => pc.addTrack(track, localStream));
 
     const offer = await pc.createOffer();
-    console.log('start => offer', offer);
-    console.log('start => pc.setLocalDescription(offer)');
     await pc.setLocalDescription(offer);
-
-    console.log("start => pc", pc);
-    console.log("start => pc.localDescription", pc.localDescription);
-    console.log("start => socket.emit('signal', { description: pc.localDescription }");
     socket.emit('signal', { description: pc.localDescription });
 }
 
@@ -38,8 +32,6 @@ function createPeerConnection() {
     pc = new RTCPeerConnection(configuration);
 
     pc.onicecandidate = ({ candidate }) => {
-        console.log('createPeerConnection => onicecandidate => candidate', candidate);
-        console.log("createPeerConnection => onicecandidate => socket.emit('signal', { candidate })");
         socket.emit('signal', { candidate });
     };
 
@@ -56,32 +48,17 @@ function createPeerConnection() {
 }
 
 socket.on('signal', async (data) => {
-    console.log('on("signal") => data', data);
-
     if (data.description) {
-        console.log('on("signal") => data.description', data.description);
         const remoteDesc = new RTCSessionDescription(data.description);
-        console.log('on("signal") => remoteDesc', remoteDesc);
-
-        console.log('on("signal") => pc.setRemoteDescription(remoteDesc)');
         await pc.setRemoteDescription(remoteDesc);
 
         if (remoteDesc.type === 'offer') {
-            const answer = await pc.createAnswer();
-            console.log('on("signal") => answer', answer);
-
-            console.log('on("signal") => pc.setLocalDescription(answer)');
-            await pc.setLocalDescription(answer);
-
-            console.log('on("signal") => "signal", pc', pc);
-            console.log('on("signal") => "signal", pc.localDescription', pc.localDescription);
-            console.log('on("signal") => "signal", { description: pc.localDescription }');
-            socket.emit('signal', { description: pc.localDescription });
+          const answer = await pc.createAnswer();
+          await pc.setLocalDescription(answer);
+          socket.emit('signal', { description: pc.localDescription });
         }
     } else if (data.candidate) {
-        console.log('on("signal") => data.candidate', data.candidate);
         try {
-            console.log('pc.addIceCandidate(data.candidate)');
             await pc.addIceCandidate(data.candidate);
         } catch (e) {
             console.error('Ошибка при добавлении ICE-кандидата', e);
@@ -93,14 +70,7 @@ async function restartIce() {
     console.log('Перезапуск ICE');
     const offer = await pc.createOffer({ iceRestart: true });
 
-    console.log('restartIce => offer', offer);
-    console.log('restartIce => pc.setLocalDescription(offer)');
-
     await pc.setLocalDescription(offer);
-
-    console.log('restartIce => pc', pc);
-    console.log('restartIce => pc.localDescription', pc.localDescription);
-    console.log("restartIce => socket.emit('signal', { description: pc.localDescription })");
 
     socket.emit('signal', { description: pc.localDescription });
 }
